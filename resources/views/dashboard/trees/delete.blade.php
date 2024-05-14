6fb0ca36a6f16295e2b77e68c17f65d901d12178
naim114 <naim.muhammad99@gmail.com>
2024-05-14 10:03:28 +0800

    Delete trees function

--- resources/views/dashboard/trees/delete.blade.php
+++ resources/views/dashboard/trees/delete.blade.php
@@ -5,77 +5,84 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <form>
+            <form method="POST" action="{{ route('trees.delete') }}">
+                @csrf
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">BlockX</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="id" type="number" class="form-control" value="{{ $tree->id }}" hidden>
+                        <input name="BlockX" type="number" class="form-control" value="{{ $tree->BlockX }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">BlockY</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="BlockY" type="number" class="form-control" value="{{ $tree->BlockY }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">x</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="x" type="number" class="form-control" value="{{ $tree->x }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">y</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="y" type="number" class="form-control" value="{{ $tree->y }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">TreeNum</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="TreeNum" type="text" class="form-control" value="{{ $tree->TreeNum }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">species</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="species" type="text" class="form-control" value="{{ $tree->species }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">species_groups</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="species_groups" type="text" class="form-control" value="{{ $tree->species_groups }}"
+                            readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">diameter_dbh_cm</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="diameter_dbh_cm" type="number" step=".01" class="form-control"
+                            value="{{ $tree->diameter_dbh_cm }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">diameter_class</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="diameter_class" type="number" class="form-control"
+                            value="{{ $tree->diameter_class }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">stem_height_m</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="stem_height_m" type="number" step=".01" class="form-control"
+                            value="{{ $tree->stem_height_m }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">volume_m3</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="volume_m3" type="number" step=".01" class="form-control"
+                            value="{{ $tree->volume_m3 }}" readonly>
                     </div>
                 </div>
                 <div class="form-group row mb-4">
                     <label class="col-sm-2 col-form-label">Status</label>
                     <div class="col-sm-10">
-                        <input name="" type="text" class="form-control">
+                        <input name="status" type="text" class="form-control" value="{{ $tree->status }}" readonly>
                     </div>
                 </div>
 
